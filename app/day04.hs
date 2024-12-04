5e56cc92a019a8722ffcc178166e2fabb804b271
Jianbo Liu <jliu32@nd.edu>
2024-12-04 17:50:24 -0500

    Refactor day04

--- app/day04.hs
+++ app/day04.hs
@@ -1,64 +1,48 @@
 module Main where
 
 import Criterion.Main
-import Data.List (isPrefixOf)
 import Utils (readInputLines)
 
-rows :: Int
+rows, columns :: Int
 rows = 140
-
-columns :: Int
 columns = 140
 
+isValidPos :: [String] -> (Int, Int) -> Char -> Bool
+isValidPos matrix (r, c) ch = r >= 0 && c >= 0 && r < rows && c < columns && matrix !! r !! c == ch
+
 check :: [String] -> String -> (Int, Int) -> (Int, Int) -> Bool
-check matrix word (sr, sc) (rd, cd) =
-  let positions = [(sr + i * rd, sc + i * cd) | i <- [0 .. length word - 1]]
-      chars = [matrix !! r !! c | (r, c) <- positions, r >= 0, c >= 0, r < rows, c < columns]
-   in word `isPrefixOf` chars
+check matrix word (sr, sc) (dr, dc) =
+  all (\(i, ch) -> isValidPos matrix (sr + i * dr, sc + i * dc) ch) (zip [0 ..] word)
 
 findall :: [String] -> String -> Int
 findall matrix word =
-  sum $ [fromEnum (check matrix word (r, c) dir) | r <- [0 .. rows - 1], c <- [0 .. columns - 1], dir <- directions]
-  where
-    directions = [(0, 1), (1, 0), (0, -1), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+  sum $
+    [ fromEnum (check matrix word (r, c) dir)
+      | (r, row) <- zip [0 ..] matrix,
+        (c, char) <- zip [0 ..] row,
+        char == head word,
+        dir <- [(0, 1), (1, 0), (0, -1), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+    ]
 
 part1 :: [String] -> Int
 part1 input = findall input "XMAS"
 
 checkxmas :: [String] -> Int
 checkxmas matrix =
-  let positions = [(ar, ac) | (ar, row) <- zip [0 ..] matrix, (ac, char) <- zip [0 ..] row, char == 'A']
-   in sum $ map (fromEnum . ismax) positions
-  where
-    ismax (r, c) =
-      any
-        ( \(d1, d2) ->
-            let (d1r, d1c) = (r + d1, c + d1)
-                (d2r, d2c) = (r + d2, c - d2)
-                (d3r, d3c) = (r - d1, c - d1)
-                (d4r, d4c) = (r - d2, c + d2)
-             in d1r >= 0
-                  && d1c >= 0
-                  && d2r >= 0
-                  && d2c >= 0
-                  && d3r >= 0
-                  && d3c >= 0
-                  && d4r >= 0
-                  && d4c >= 0
-                  && d1r < rows
-                  && d1c < columns
-                  && d2r < rows
-                  && d2c < columns
-                  && d3r < rows
-                  && d3c < columns
-                  && d4r < rows
-                  && d4c < columns
-                  && matrix !! d1r !! d1c == 'M'
-                  && matrix !! d3r !! d3c == 'S'
-                  && matrix !! d2r !! d2c == 'M'
-                  && matrix !! d4r !! d4c == 'S'
-        )
-        [(-1, -1), (-1, 1), (1, -1), (1, 1)]
+  sum
+    [ fromEnum $
+        all
+          (\((nr, nc), ch) -> isValidPos matrix (nr, nc) ch)
+          [ ((r + dr, c + dc), 'M'),
+            ((r - dr, c - dc), 'S'),
+            ((r + dr, c - dc), 'M'),
+            ((r - dr, c + dc), 'S')
+          ]
+      | (r, row) <- zip [0 ..] matrix,
+        (c, char) <- zip [0 ..] row,
+        char == 'A',
+        (dr, dc) <- [(-1, -1), (-1, 1), (1, -1), (1, 1)]
+    ]
 
 part2 :: [String] -> Int
 part2 = checkxmas
